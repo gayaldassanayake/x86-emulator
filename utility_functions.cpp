2d53734604fc2a204d870dd395fad6a7eb70d2f1
Damika Gamlath <damika97.10@gmail.com>
2021-10-19 17:26:32 +0530

    add opcodes opcode selection in main

--- utility_functions.cpp
+++ utility_functions.cpp
@@ -11,7 +11,8 @@ uint32_t readDispalcement(Reader *reader, uint32_t disp_bytes){
     uint32_t displacement = 0;
     for(uint32_t i=0;i<disp_bytes;i++){
         uint32_t tmp = reader->readNextByte();
-        displacement = (displacement<<(8*i)) | tmp;
+        // displacement = (displacement<<(8*i)) | tmp;
+        displacement = (tmp<<(8*i)) | displacement;
     }
     return displacement;
 }
